8280df3e3f3638a6331915578fbaa58edfb113f5
Naveen Adepu <sainaveenadepu@gmail.com>
2024-04-16 12:43:07 +0530

    python_basic

---
+++ normal_class.py
@@ -0,0 +1,25 @@
+class Bookshelf:
+    def __init__(self, quantity):
+        self.quantity = quantity
+
+    def __str__(self):
+        return f"Bookshelf with {self.quantity} books."
+    
+shelf = Bookshelf(300)
+print(shelf)
+
+
+class Car:
+    def __init__(self,name, make, year):
+        self.name = name
+        self.make = make
+        self.year = year
+    def __str__(self):
+        return f"what is the name of the car? {self.name} and make of the car is {self.make}, and in whcih year car is manufactured {self.year}"
+    
+
+car_details = Car("ford","Enduover", 2024)
+
+print(car_details)
+
+
