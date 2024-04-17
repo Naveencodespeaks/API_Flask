3fc3aee40162fdd8b17267f5ea9aad3758a864c9
Naveen Adepu <sainaveenadepu@gmail.com>
2024-04-17 09:52:45 +0530

    python_basic

---
+++ error.py
@@ -0,0 +1,11 @@
+def divide(dividend, divisor):
+    if divisor == 0:
+        raise ZeroDivisionError("Divisor cannot be 0.")
+    
+    return dividend/ divisor
+
+grades = []
+
+print("Welcome to the average grade program")
+average = divide(sum(grades), len(grades))
+print(f"The average grade is {average}")
