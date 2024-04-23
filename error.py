cfc4c5422937d6fa4e201896fe834e4481fb9b2c
Naveen Adepu <sainaveenadepu@gmail.com>
2024-04-23 11:51:37 +0530

    python basic

--- error.py
+++ error.py
@@ -1,11 +1,23 @@
 def divide(dividend, divisor):
     if divisor == 0:
-        raise ZeroDivisionError("Divisor cannot be 0.")
+        raise ZeroDivisionError("Divisor cannot be 0 ")
+        
+    return dividend / divisor
+
+
+grade = []
+print("Welcome to the average calculator. ")
+try:
+    average = divide(sum(grade),len(grade))
+    print(f"The average grade is {average}")
     
-    return dividend/ divisor
+except ZeroDivisionError as e :
+    print("These are no grades yet in your list. ")   
+else:
+    print(f"The average grade is {average}")
+finally:
+    print("Thank you")
 
-grades = []
 
-print("Welcome to the average grade program")
-average = divide(sum(grades), len(grades))
-print(f"The average grade is {average}")
+
+
