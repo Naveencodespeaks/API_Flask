84daa966a05051c924947faa96ce015c5443d872
Naveen Adepu <sainaveenadepu@gmail.com>
2024-04-26 11:19:03 +0530

    python_basic

---
+++ class.py
@@ -0,0 +1,13 @@
+class Car:
+    def __init__(self,name, make, model,  milage, price):
+        self.name = name 
+        self.make = make
+        self.model = model
+        self.milage = milage
+        self.price = price
+
+
+car_details = Car('ford',2024,'Endeavour','25kml','30lakhs' )
+print(car_details)
+
+
